83e000cd0adc3050051128cfebe28a80dd2dbd65
Fernando Lopez <fernandolopezgallo@Fernandos-MacBook-Air.local>
2025-11-03 18:01:39 -0500

    fix: usar aws-amplify v5 y corregir import de Auth

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsConfig from './aws-config';
 import reportWebVitals from './reportWebVitals';
 
