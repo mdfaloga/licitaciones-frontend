f9b8fa037caf4e45c14557ff154b03f85daf3d56
Fernando Lopez <fernandolopezgallo@Fernandos-MacBook-Air.local>
2025-11-03 15:37:29 -0500

    modificacion App.js

--- src/App.js
+++ src/App.js
@@ -7,22 +7,6 @@ function App() {
       <h1>Bienvenido a Licitaciones Colombia</h1>
       <p>Explora convocatorias públicas filtradas por tus criterios.</p>
     </div>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
